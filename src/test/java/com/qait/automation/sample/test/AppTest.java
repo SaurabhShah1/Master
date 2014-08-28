5ecd3d77443d44c372f1925d60163438f29d7c6e
saurabhshah1 <saurabhshah@LNDA0110.qait.com>
2014-08-28 19:21:53 +0530

    First Commit

---
+++ src/test/java/com/qait/automation/sample/test/AppTest.java
@@ -0,0 +1,19 @@
+package com.qait.automation.sample.test;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+
+public class AppTest {
+
+	@BeforeClass
+	public void setUp() throws Exception {
+		System.out.println("Saurabh Shah");
+	}
+
+@Test
+	public void test() {
+	System.out.println("Saurabh Shah");
+	}
+}
